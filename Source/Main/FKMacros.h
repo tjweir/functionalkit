838dc61757923979a7b32aa82d2af3dfe9274ab1
Nick Partridge <nkpart@gmail.com>
2009-03-17 10:16:43 +1000

    added ifdef checks on macros

--- Source/Main/FKMacros.h
+++ Source/Main/FKMacros.h
@@ -1,8 +1,23 @@
+#ifndef ATTR
 #define ATTR @property (nonatomic, retain)
+#endif
+
+#ifndef READ
 #define READ @property (readonly)
+#endif
 
+#ifndef NSARRAY
 #define NSARRAY(args...) [NSArray arrayWithObjects:args, nil]
-#define EMPTY_ARRAY [NSArray array]
+#endif
 
+#ifndef EMPTY_ARRAY
+#define EMPTY_ARRAY [NSArray array]
+#endif
+
+#ifndef NSDICT
 #define NSDICT(args...) [NSDictionary dictionaryWithObjectsAndKeys:args, nil]
+#endif
+
+#ifndef EMPTY_DICT
 #define EMPTY_DICT [NSDictionary dictionary]
+#endif

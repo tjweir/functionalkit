0ec77933636c2625b2ea30021d74fae11fc49c40
Tom Adams <tomjadams@gmail.com>
2009-03-19 15:34:47 +1000

    Added euqlaus, hash, description, etc.

--- Source/Main/FKFunction.m
+++ Source/Main/FKFunction.m
@@ -1,13 +1,20 @@
 #import "FKFunction.h"
 #import "MVMacros.h"
+#import "FKMacros.h"
 
 @interface FKFunctionFromSelector : NSObject <FKFunction> {
 	SEL selector;
 }
+
+READ SEL selector;
+
 - (FKFunctionFromSelector *)initWithSelector:(SEL)s;
 @end
 
 @implementation FKFunctionFromSelector
+
+@synthesize selector;
+
 - (FKFunctionFromSelector *)initWithSelector:(SEL)s {
 	if ((self = [super init])) {
 		selector = s;
@@ -18,16 +25,38 @@
 - (id):(id)arg {
 	return [arg performSelector:selector];
 }
+
+#pragma mark NSObject methods.
+- (BOOL)isEqual:(id)object {
+    return object == nil || ![[object class] isEqual:[FKFunctionFromSelector class]] ? NO : FKEqualSelectors(self.selector, ((FKFunctionFromSelector *) object).selector);
+}
+
+- (NSUInteger)hash {
+    return 42;
+}
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"<%@ selector: %s>", [self className], sel_getName(selector)];
+}
+
 @end
 
 @interface FKFunctionFromSelectorWithTarget : NSObject <FKFunction> {
 	SEL selector;
 	NSObject *target;
 }
+
+READ SEL selector;
+READ NSObject *target;
+
 - (FKFunctionFromSelectorWithTarget *)initWithSelector:(SEL)s target:(NSObject *)target;
+
 @end
 
 @implementation FKFunctionFromSelectorWithTarget
+
+@synthesize selector, target;
+
 - (FKFunctionFromSelectorWithTarget *)initWithSelector:(SEL)s target:(NSObject *)nTarget {
 	if (![nTarget respondsToSelector:s]) {
 		@throw [NSException exceptionWithName:NSInvalidArgumentException reason:[NSString stringWithFormat:@"target %@ does not respond to selector %s", nTarget, sel_getName(s)] userInfo:EMPTY_DICT];
@@ -42,6 +71,30 @@
 - (id):(id)arg {
 	return [target performSelector:selector withObject:arg];
 }
+
+#pragma mark NSObject methods.
+- (void) dealloc {
+    [target release];
+    [super dealloc];
+}
+
+- (BOOL)isEqual:(id)object {
+    if (object == nil || ![[object class] isEqual:[FKFunctionFromSelectorWithTarget class]]) {
+        return NO;
+    } else {
+        FKFunctionFromSelectorWithTarget *other = (FKFunctionFromSelectorWithTarget *) object;
+        return FKEqualSelectors(self.selector, other.selector) && [self.target isEqual:other.target];
+    }
+}
+
+- (NSUInteger)hash {
+    return 42;
+}
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"<%@ selector: %s; target: %@>", [self className], sel_getName(selector), target];
+}
+
 @end
 
 @implementation FKFunction
--- Source/Main/FKMacros.h
+++ Source/Main/FKMacros.h
@@ -21,3 +21,19 @@
 #ifndef EMPTY_DICT
 #define EMPTY_DICT [NSDictionary dictionary]
 #endif
+
+#if OBJC_API_VERSION >= 2
+
+// With Objective-C 2.0 runtime, we can compare using runtime-provided function
+#import <objc/runtime.h>
+FOUNDATION_STATIC_INLINE BOOL FKEqualSelectors(SEL a, SEL b) {
+	return sel_isEqual(a, b);
+}
+#else
+
+// Without Objective-C 2.0 runtime, just use pre-Leopard comparison
+FOUNDATION_STATIC_INLINE BOOL FKEqualSelectors(SEL a, SEL b) {
+	return a == b;
+}
+
+#endif
